82aa129a6a97dbb08fcd96b7fced456c9353553d
Matei <info.claudiu@gmail.com>
2021-04-17 22:06:24 +0200

    Add amount of persons input

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      nameInput: '',
       listOne: []
     };
 
@@ -13,17 +14,21 @@ class App extends Component {
   }
 
   handleChange(event) {
-    this.setState({value: event.target.value});
+    let value = event.target.value;
+    console.log(event.target.name);
+    console.log(value);
+    this.setState({
+      ...this.state,
+      [event.target.name]: value
+    }, () => console.log(this.state));
   }
 
   handleSubmit(event) {
+    this.state.listOne.push({familyName: this.state.nameInput,amount : this.state.amount })
+    this.setState({...this.state});
+    alert('Familia ' + this.state.nameInput + ' este adaugata la lista de duminica dimineata');
+    console.log(this.state);
     event.preventDefault();
-
-    if (this.state.value === '' || this.state.value === undefined) return
-
-    this.state.listOne.push(this.state.value)
-    this.setState({value: this.state.value});
-    alert('Familia ' + this.state.value + ' este adaugata la lista de duminica dimineata');
   }
 
   render() {
@@ -31,16 +36,24 @@ class App extends Component {
       <>
         <h2>Lista de dimineață</h2>
         <form onSubmit={this.handleSubmit}>
-          <label>
-            Numele de familie:<br/>
-            <input type="text" value={this.state.value} onChange={this.handleChange}/>
-          </label>
+          <div>
+            <label>
+              Numele de familie:<br/>
+              <input type="text" name="nameInput" value={this.state.nameInput} onChange={this.handleChange}/>
+            </label>
+          </div>
+          <div>
+            <label>
+              Numărul persoanelor peste 12 ani:<br/>
+              <input type="number" name="amount" value={this.state.amount} onChange={this.handleChange}/>
+            </label>
+          </div>
           <input type="submit" value="Submit"/>
         </form>
         <div>
           <ul>
-            {this.state.listOne.map(item => (
-              <li key={item}>{item}</li>
+            {this.state.listOne.map((item, index) => (
+              <li key={index}>Familia {item.familyName} {item.amount} persoane</li>
             ))}
           </ul>
         </div>
